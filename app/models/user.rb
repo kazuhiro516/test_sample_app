0f22cf1b920647eb0514c28afd07008d761ea9d2
kurokawakazuhiro <k.kurokawa11@gmail.com>
2021-04-03 07:59:21 +0900

    メールアドレスを全て小文字に変換する処理を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+  # db登録直前にemailを全て小文字に変換する処理
+	before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   # メールアドレスのバリデーションを設定
   # フォーマットと一意であることの確認
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
   			format: { with: VALID_EMAIL_REGEX }
-  			uniqueness: { case_sensitive: false }
+  			uniqueness: true
 end
